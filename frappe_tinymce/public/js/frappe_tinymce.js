2dda4019884b9258ec47c5656b0a550d827be312
Ardan Peddell <ardan@corebpm.com>
2023-02-01 15:38:27 +0800

    Changes to Menu bar
    Updates to Menu etc

--- frappe_tinymce/public/js/frappe_tinymce.js
+++ frappe_tinymce/public/js/frappe_tinymce.js
@@ -17,19 +17,26 @@ frappe.ui.form.ControlTextEditor = class ControlTextEditor extends frappe.ui.for
         this.quill_container = $('<div>').appendTo(this.input_area);
         tinymce.init({
             target: this.input_area,
-            toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment | footnotes | mergetags',
-            font_size_formats: '10px 11px 12px 14px 15px 16px 18px 24px 36px',
+            toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist  | forecolor backcolor removeformat | pagebreak | charmap media link | image',font_size_formats: '10px 11px 12px 14px 15px 16px 18px 24px 36px',
             plugins: [
               'autoresize', 'autolink', 'charmap', 'emoticons', 'fullscreen', 'help',
-              'image', 'link', 'lists', 'searchreplace',
-              'table', 'visualblocks', 'visualchars', 'wordcount',
+              'image', 'media', 'link', 'lists', 'searchreplace',
+              'table', 'visualblocks', 'visualchars',
             ],
+            paste_data_images: true,
+            menubar: 'edit view insert format table',
             powerpaste_googledocs_import: "prompt",
             entity_encoding: 'raw',
             convert_urls: true,
             content_css: false,
             toolbar_sticky: true,
             promotion: false,
+            statusbar: false,
+            // without images_upload_url set, Upload tab won't show up
+            images_upload_url: 'upload.php',
+            
+            // override default upload handler to simulate successful upload
+            images_upload_handler: image_upload_handler_callback,
 
             setup: function(editor) {
                 that.editor_id = editor.id
